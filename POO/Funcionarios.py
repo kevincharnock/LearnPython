fbe8c3fa65ddf8ee86d31a01d0a6338bee308eb4
Kevin Charnock <kevinshuldiner@gmail.com>
2023-03-21 22:51:58 -0300

    adding poo

---
+++ POO/Funcionarios.py
@@ -0,0 +1,14 @@
+
+#Create a Class
+class Funcionarios:
+    pass
+
+#Create a Object
+fun1 = Funcionarios()
+
+#Parameters
+fun1.nome = 'kevin'
+fun1.sobrenome = 'charnock'
+fun1.dataNascimento = '16/01/1998'
+
+print(fun1.nome)
